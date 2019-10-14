7bb84f0a41059acb437040a16a774c91537fc00b
nisha-gupta2k19 <55271116+nisha-gupta2k19@users.noreply.github.com>
2019-10-13 23:48:48 -0700

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+<title>admin</title>
+</head><body bgcolor="orange">
+<?php
+include "menu.php";
+?>
+<p><br>
+<p><br>
+<form action="admindb.php" method="POST" align="center" margin="70%">
+<center><b><font size="60" >Admin </font></center></b><br><p>
+<br><p>
+<table align="center">
+<tr>
+<td colspan="2" border="1">
+<h2>User name:<input type="text" name="un"><br></h2>
+</td>
+</tr>
+<tr>
+<td colspan="2">
+<h2>Password:<input type="password" name="pwd"><br></h2>
+</td>
+</tr>
+<tr>
+<td colspan="2">
+<input type="submit" value="submit"></td>
+</tr>
+
+</table>
+</form>
+</form>
+</body>
+</html>
